0fb6b62de8cb0838ad604c4731d3dc3c8054201e
Stefania Galazzo <galazzostefania@gmail.com>
2021-10-27 21:38:13 +0200

    esercizio 27 ottobre 2021

--- 27-ottobre-2021/script.js
+++ 27-ottobre-2021/script.js
@@ -30,13 +30,13 @@ function countParagraph() {
 
 
 // Animated bar
-var i = 0;
+let i = 0;
 function move() {
   if (i == 0) {
     i = 1;
-    var elem = document.getElementById("myBar");
-    var width = 1;
-    var id = setInterval(frame, 10);
+    let elem = document.getElementById("myBar");
+    let width = 1;
+    let id = setInterval(frame, 10);
     function frame() {
       if (width >= 100) {
         clearInterval(id);
