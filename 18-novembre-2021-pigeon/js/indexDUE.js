b8cfc375e514672de91324411ba078b98416e527
Stefania Galazzo <galazzostefania@gmail.com>
2021-11-20 10:54:48 +0100

    pigeon game v0.1

--- 18-novembre-2021-pigeon/js/indexDUE.js
+++ 18-novembre-2021-pigeon/js/indexDUE.js
@@ -86,7 +86,24 @@ document.addEventListener('DOMContentLoaded', () => {
     
     });
     
-    });
+    document.addEventListener('keyup', event => {
+        const cacca = document.querySelector('.cacca');
+        if (event.code === 'Space') {
+            // cacca.style.display = "block";
+            setTimeout(() => {
+                cacca.style.zIndex = "1";
+            }, 10)
+
+            setTimeout(() => {
+                cacca.style.top = "800px";
+            }, 180)
+
+            setTimeout(()=>{
+                clearInterval();
+                },1500);
+        }
+      })
+});
 
 
 
@@ -111,79 +128,3 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-
-
-// const up = document.querySelector('#up');
-// const dx = document.querySelector('#dx');
-// const dwn = document.querySelector('#dwn');
-// const sx = document.querySelector('#sx');
-
-// const base = document.querySelector(".base");
-// const floor = document.querySelector(".floor");
-// const prot = document.querySelector(".prot");
-
-// let backgroundBoard = 0;
-// let backgroundFloor = 0;
-
-// dx.addEventListener('click', () => {
-//     prot.classList.remove('sx');
-//     base.style.backgroundPositionX = `${backgroundBoard -= 3}px`;
-//     floor.style.backgroundPositionX = `${backgroundFloor -= 20}px`;
-
-//     prot.classList.toggle('move');
-//     setTimeout(() => { prot.classList.toggle('move') }, 250)
-// })
-
-// sx.addEventListener('click', () => {
-//     prot.classList.add('sx');
-//     base.style.backgroundPositionX = `${backgroundBoard += 3}px`;
-//     floor.style.backgroundPositionX = `${backgroundFloor += 20}px`;
-
-//     prot.classList.toggle('move');
-//     setTimeout(() => { prot.classList.toggle('move') }, 250)
-// })
-
-
-// up.addEventListener('click', () => {
-//     prot.classList.toggle('animate__bounce');
-//     setTimeout(() => { prot.classList.toggle('animate__bounce') }, 2000)
-// })
-
-// document.onkeydown = arrowPress;
-
-// function arrowPress(event) {
-//     switch (event.keyCode) {
-//         case 39:
-//             dxMove();
-//             break;
-//         case 37:
-//             sxMove();
-//             break;
-//         case 38:
-//             upMove()
-//             break;
-//     }}
-
-
-//     const dxMove = () => {
-//         prot.classList.remove('sx');
-//         base.style.backgroundPositionX = `${backgroundBoard -= 3}px`;
-//         floor.style.backgroundPositionX = `${backgroundFloor -= 20}px`;
-
-//         prot.classList.toggle('move');
-//         setTimeout(() => { prot.classList.toggle('move') }, 250)
-//     }
-
-//     const sxMove = () => {
-//         prot.classList.add('sx');
-//         base.style.backgroundPositionX = `${backgroundBoard += 3}px`;
-//         floor.style.backgroundPositionX = `${backgroundFloor += 20}px`;
-
-//         prot.classList.toggle('move');
-//         setTimeout(() => { prot.classList.toggle('move') }, 250)
-//     }
-
-//     const upMove = () =>{
-//         prot.classList.toggle('animate__bounce');
-//         setTimeout(() => { prot.classList.toggle('animate__bounce') }, 1000)
-//     }
